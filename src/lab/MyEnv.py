fix camera follower motion and modified reward computation

src/lab/MyEnv.py
@@ -20,6 +20,8 @@ class MyEnv(ManagerBasedRLEnv):
         super().__init__(cfg, render_mode, **kwargs)
 
         self.camera_controller = ViewportCameraController(self, cfg= ViewerCfg(origin_type='world'))
+        self.old_eye_pos = None
+        self.old_lookat_pos = None
 
         map_png = str(SCENE_USD_PATH.parent / SCENE_USD_PATH.stem) + "_map.png"
         map_yaml = str(SCENE_USD_PATH.parent / SCENE_USD_PATH.stem) + "_map.yaml"
@@ -174,12 +176,24 @@ class MyEnv(ManagerBasedRLEnv):
         robot_pos = self.scene['unitree_go2'].data.root_pos_w[0]
         robot_quat = self.scene['unitree_go2'].data.root_quat_w[0]
 
-        camera_offset_local = torch.tensor([-1.2, 0.0, 1.2], device=self.device)
-        lookat_offset_local = torch.tensor([2.0, 0.0, -1.0], device=self.device)
+        camera_offset_local = torch.tensor([-1.0, 0.0, 1.0], device=self.device)
+        lookat_offset_local = torch.tensor([2.0, 0.0, -0.8], device=self.device)
 
         camera_offset_w = quat_apply(robot_quat, camera_offset_local)
         lookat_offset_w = quat_apply(robot_quat, lookat_offset_local)
 
-        eye_pos = robot_pos + camera_offset_w
-        lookat_pos = robot_pos + lookat_offset_w
-        self.camera_controller.update_view_location(eye=eye_pos.cpu().numpy(), lookat=lookat_pos.cpu().numpy())
+        if self.old_eye_pos is None:
+            self.old_eye_pos = robot_pos + camera_offset_w
+            self.old_lookat_pos = robot_pos + lookat_offset_w
+        else:            # smooth camera movement by interpolating between old and new positions
+            alpha = 0.2  # smoothing factor
+            new_eye_pos = robot_pos + camera_offset_w
+            new_lookat_pos = robot_pos + lookat_offset_w
+
+            smoothed_eye_pos = (1 - alpha) * self.old_eye_pos + alpha * new_eye_pos
+            smoothed_lookat_pos = (1 - alpha) * self.old_lookat_pos + alpha * new_lookat_pos
+
+            self.camera_controller.update_view_location(eye=smoothed_eye_pos.cpu().numpy(), lookat=smoothed_lookat_pos.cpu().numpy())
+
+            self.old_eye_pos = smoothed_eye_pos
+            self.old_lookat_pos = smoothed_lookat_pos

src/lab/reward_helpers.py
@@ -10,8 +10,8 @@ from lab.observation_helpers import get_path_obs, get_lidar
 
 
 def dist_to_goal_xy(env: MyEnv) -> torch.Tensor:
-    robot = env.scene["unitree_go2"]
-    pos_w = robot.data.root_pos_w
+    robot: Articulation = env.scene["unitree_go2"]
+    pos_w = robot.data.root_com_pos_w
     goal_local = env.goal_pos
     goal_w = goal_local + env.scene.env_origins[:, :2]
     delta_xy = goal_w[:, 0:2] - pos_w[:, 0:2]
@@ -39,22 +39,22 @@ def penalty_collision_base(env: MyEnv, force_thresh: float = 3.0, penalty: float
     mag = torch.linalg.norm(forces, dim=-1).max(dim=-1).values
     return torch.where(mag > force_thresh, torch.full_like(mag, penalty), torch.zeros_like(mag))
 
-def penalty_obstacle_proximity(env: MyEnv, dist_thresh: float = 0.4, penalty: float = -2.0) -> torch.Tensor:
+def penalty_obstacle_proximity(env: MyEnv, max_penalty: float = -2.0) -> torch.Tensor:
     lidar_ranges = get_lidar(env, num_obstacles=10, normalize=False)  # (B, num_obstacles*2)
     min_dist = lidar_ranges[:, :10].min(dim=-1).values  # (B,)
-    return torch.where(min_dist < dist_thresh, torch.full_like(min_dist, penalty), torch.full_like(min_dist, -penalty))
+    return max_penalty + torch.clamp(torch.pow(min_dist, 2), min = max_penalty, max=-max_penalty) # ()
 
 def robot_heading_reward(env: MyEnv) -> torch.Tensor:
     # Returns 1 if robot is heading in the correct direction, 0 otherwise
     # We compute the alignment wrt to the direction of the last point of the path in hour horizon.
     obs_tensor = get_path_obs(env, num_points_forward=10, normalize=True)  # (B, 20)
-    last_heading = obs_tensor[:, -1]
-    # if last_heading == 0.5 -> robot is facing the goal. Max reward.
-    heading_delta = torch.abs(0.5 - last_heading)
+    dist, headings = obs_tensor[:, :10], obs_tensor[:, 10:]  # (B, 10), (B, 10)
+    next_heading = headings[:, 2] # I extract the heading of 3rd point forward (approx 1m ahead)
+    # if next_heading == 0.5 -> robot is facing the correct direction. Max reward.
+    heading_delta = torch.abs(0.5 - next_heading)
     return 1.0 - heading_delta * 2.0  # normalize to [0, 1]
 
-def smoothness_reward(env: MyEnv, alpha: float = 0.1) -> torch.Tensor:
+def action_smoothness_penalty(env: MyEnv, alpha: float = 0.2) -> torch.Tensor:
     # I assume that jerky motions are correlated to sudden changes in velocity.
-    robot:Articulation = env.scene["unitree_go2"]
-    delta_v = (robot.data.root_com_vel_w - env._last_vel)
-    return -alpha * torch.linalg.norm(delta_v, dim=-1)
+    delta = env._action_buffer[:, 0] - env._action_buffer[:, 1]  # (B, action_dim)
+    return -alpha * torch.linalg.norm(delta, dim=-1)

src/policy_model/ActorCriticWithEncoders.py
@@ -94,7 +94,7 @@ class ActorCriticWithEncoders(ActorCritic):
             obs_size = obs_tensor.shape[-1]
             if encoder_cfg is not None and obs_name in encoder_cfg:  # initialize an encoder for this observation term
                 hidden_dims = encoder_cfg[obs_name]
-                encoders[obs_name] = MLP(obs_size, hidden_dims[-1], hidden_dims[:-1], activation)
+                encoders[obs_name] = MLP(obs_size, hidden_dims[-1], hidden_dims[:-1], activation, last_activation='sigmoid')
                 input_dim += hidden_dims[-1]
             else:
                 encoders[obs_name] = nn.Identity()